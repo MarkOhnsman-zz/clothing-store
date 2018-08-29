5f8e53c5a7a9cb940dd41d825aca61e073b148e8
Mark Ohnsman <mark.ohnsman@gmail.com>
2018-08-29 11:47:21 -0600

    router prep

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Learn more about Codeworks clothes</h1>
+    <h3> OUR GOALS: </h3>
+    <ul>
+      <li>Efficiently unleash cross-media information without cross-media value.</li>
+      <li>Quickly maximize timely deliverables for real-time schemas.</li>
+      <li>Dramatically maintain clicks-and-mortar solutions without functional solutions.</li>
+    </ul>
   </div>
 </template>
